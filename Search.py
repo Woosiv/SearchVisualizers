dc89f0c0256e6e49fce1a6955c1138953871f8e9
Woosiv <williamhsiehoct@gmail.com>
2021-03-27 16:19:21 -0700

    Added a cost update for nodes

--- Search.py
+++ Search.py
@@ -3,8 +3,8 @@ import timeit
 import time
 from collections import deque
 grid = np.array([1,1])
-start = [1,1]
-end = [3,3]
+start = (1,1)
+end = (3,3)
 
 # Initializes the initial grid for the search methods, based off if its 
 # a cost based search or basic search
@@ -35,10 +35,10 @@ def init_grid(cost = True):
     if cost:
         grid = np.random.randint(low, high, (x,y))
     else:
-        grid = np.zeros((x,y))
+        grid = np.ones((x,y))
     
 # Finds surrounding indices with the given position
-def surroundIndices(pos, bounds, visited=None, omni=True) -> list:
+def surroundIndices(pos, bounds, visited=None, omni=False) -> list:
     # If the search decides to not remembered visited nodes
     if visited == None:
         visited = set()
@@ -84,38 +84,49 @@ def surroundIndices(pos, bounds, visited=None, omni=True) -> list:
 def firstSearch(breadth = True) -> list:
     #global grid
     # position, parent
-    queue = deque([(start, None)])
+    queue = deque([start])
+    # [0] holds parents, [1] holds cost
+    parents = {start : (tuple(), 0)}
     visited = set()
     while queue:
         #print(queue)
         curr = queue.popleft() 
         # time.sleep(2)
-        pos = curr[0]
+        pos = (curr[0], curr[1])
         if pos == end:
-                # Build the path
-                path = []
-                while curr[1]:
-                    #print(path)
-                    path = [curr[0]] + path
-                    curr = curr[1]
-                path = [start] + path
-                # visitedArray = np.array([list(x) for x in visited])
-                # visitedGrid = np.zeros_like(grid)
-                # visitedGrid[visitedArray[:,0], visitedArray[:,1]] = 1
-                #print(visitedGrid)
-                #print(len(visited))
-                return path
-        elif (pos[0], pos[1]) not in visited:
-            visited.add((pos[0], pos[1]))
-            bounds = grid.shape
-            indices = [(x, curr) for x in surroundIndices(pos, bounds, visited, False)]
-            queue.extend(indices) if breadth else queue.extendleft(indices)
+            path = list(parents[pos][0]) + [pos]
+            # visitedArray = np.array([list(x) for x in visited])
+            # visitedGrid = np.zeros_like(grid)
+            # visitedGrid[visitedArray[:,0], visitedArray[:,1]] = 1
+            # print(visitedGrid)
+            #print(len(visited))
+            #print(path)
+            return path
+        elif pos not in visited:
+            visited.add(pos)
+            for x in surroundIndices(pos, grid.shape, visited):
+                # Check for duplicates
+                if x in queue:
+                    print('Proc\'d')
+                    # If the cost in the current is higher than the new found cost
+                    # remove its current queue position and update the best parent path.
+                    if parents[(x[0], x[1])][1] > parents[pos][1] + grid[x[0], x[1]]:
+                        queue.remove(x)
+                        parents[(x[0], x[1])] = (parents[pos][0] + (pos,),parents[pos][1] + grid[x[0], x[1]])
+                        queue.append(x)
+                else:
+                    parents[(x[0], x[1])] = (parents[pos][0] + (pos,), parents[pos][1] + grid[x[0], x[1]])
+                    queue.append(x) if breadth else queue.appendleft(x)
+                    
+                
+            #indices = [(x, curr) for x in surroundIndices(pos, grid.shape, visited)]
+            #queue.extend(indices) if breadth else queue.extendleft(indices)
         
     return None
 
 # Helper function to display path taken by a search
 def printResult(search, pathFound, cost):
-    print(pathFound)
+    # print(pathFound)
     res = np.zeros_like(grid)
     res[pathFound[:,0], pathFound[:,1]] = np.arange(1, len(pathFound)+1)
     print(f'Path for {search}')
@@ -128,6 +139,9 @@ if __name__ == '__main__':
     # print('Random', timeit.timeit('numpy.random.randint(0, 20, (10,10))',setup = 'import numpy', number = 1))
     # print('Zeroes', timeit.timeit('numpy.zeros((10,10))', setup = 'import numpy', number = 1))
     # Ask the user for cost or basic tree search
+    # test = np.random.randint(0, 10, (3,3))
+    # print(test)
+    # print(test[(1,1)])
     cost = input("Would you like to do cost based search or just a basic search? (Cost/Basic): ").lower()
     while cost not in ['cost', 'basic']:
         cost = input("Would you like to do cost based search or just a basic search? (Cost/Basic): ").lower()
